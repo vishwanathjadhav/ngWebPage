955972846bcf1b8b32ad30480ceddd57f95111fc
vishwanathjadhav <contrib.jvishwanath66@gmail.com>
2021-06-03 08:18:45 +0530

    Add local changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-start',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'myApp';
+  prodName = 'Mobile';
+  prodNames = ['mob', 'lapi', 'book'];
+  mobParts = [
+    {
+      id: 1001,
+      name: 'Mobile Screen',
+      inStock: 25,
+      price: 1200,
+    },
+    {
+      id: 1002,
+      name: 'Mobile Headphone',
+      inStock: 60,
+      price: 1500,
+    },
+    {
+      id: 1003,
+      name: 'Mobile Charger',
+      inStock: 125,
+      price: 1800,
+    },
+  ];
 }
